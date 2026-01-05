dfaf25c318c1b7188434c2c5f0c8bb8beae45d55
Geraa50 <gera.a130000000000000@gmail.com>
2026-01-05 18:10:57 +0600

    slingshot update 2.0

--- scripts/enemies/robot.gd
+++ scripts/enemies/robot.gd
@@ -1,7 +1,7 @@
 extends "res://scripts/enemies/AbstractEnemy.gd"
 
 
-
+@export var health = 3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,4 +13,5 @@ func _process(delta: float) -> void:
 
 #Это функция Севы, её не трожь
 func take_damage():
+	
 	pass
