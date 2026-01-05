39041e10fb5860791098de4fe0717808788dd998
Geraa50 <gera.a130000000000000@gmail.com>
2026-01-05 16:58:40 +0600

    slingshot update

--- scripts/enemies/robot.gd
+++ scripts/enemies/robot.gd
@@ -1,13 +1,16 @@
-extends "res://scripts/projectiles/AbsrtactProjectile.gd"
+extends "res://scripts/enemies/AbstractEnemy.gd"
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func on_hit():
+#Это функция Севы, её не трожь
+func take_damage():
 	pass
